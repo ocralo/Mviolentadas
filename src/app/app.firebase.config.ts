776689a7d5e50ef3eb047b7bf31a942598b80b55
ocralo <e_lopez1996@hotmail.com>
2019-05-09 20:08:35 -0500

    añadidos noevos metodos para agregar firebase

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,11 @@
+import { constants } from "perf_hooks";
+
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBkU6G-QGxzfwdSto6ZOpYXqFUjclDNFls",
+  authDomain: "vmujersdc.firebaseapp.com",
+  databaseURL: "https://vmujersdc.firebaseio.com",
+  projectId: "vmujersdc",
+  storageBucket: "vmujersdc.appspot.com",
+  messagingSenderId: "436211545918",
+  appId: "1:436211545918:web:5a684654244c27e7"
+};
